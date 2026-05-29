9c2ec4109449c6c250bebc847e68ba0e1d239fe1
TWeilong <semart2005@gmail.com>
2026-05-30 00:58:53 +0300

    Add files via upload

---
+++ HomeWorks/DataFiltering/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    public class Employee
+    {
+        public Employee() { }
+
+        public string name { get; private set; }
+        public decimal salary { get; private set; }
+        public float experience { get; private set; }
+
+        public Employee(string name, decimal salary, float experience)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.experience = experience;
+        }
+
+        public void FilterEmployees(List<Employee> employees,Predicate<Employee> isFilter)
+        {
+            foreach (var employee in employees)
+            {
+                if (isFilter(employee))
+                {
+                    Console.WriteLine($"Name: {employee.name,-10} | " + $"Experience: {employee.experience} years,-10 | " + $"Salary: {employee.salary} y.e.");
+                }
+            }
+        }
+    }
+
+}
---
+++ HomeWorks/DataFiltering/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 
+
+            var employees = new List<Employee>()
+            {
+            new Employee("Brad",    50001m, 6f),
+            new Employee("John",     4000m, 40f),
+            new Employee("Samuel",  70000m, 5.1f),
+            new Employee("Lisa",      500m, 4.5f),
+            new Employee("Michiel",150000m, 7f)
+            };
+
+            new Employee().FilterEmployees(employees,SimplePredicate);
+
+            bool SimplePredicate(Employee emp)
+            {
+                if (emp.salary > 50000m &&
+                    emp.experience > 5f)
+                    return true;
+
+                return false;
+            }
+
+            #endregion
+        }
+    }
+
+}
